bd5316d7f4115c763be7544746107c54d8f80b49
Fahad Bhatti <fahadqayyumbhatti2001@gmail.com>
2023-04-14 23:56:05 +0500

    Initial Commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import { Header } from '@/Component'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        StyreneAWebBoldItalic: 'StyreneAWeb-BoldItalic, sans-serif',
+        AdineuePROCyrBoldWeb: 'AdineuePROCyr-BoldWeb, sans-serif',
+        AdineuePROCyrLightWeb: 'AdineuePROCyr-LightWeb, sans-serif',
+        StyreneAWebLight: 'StyreneAWeb-Light, sans-serif',
+        StyreneAWebBlackItalic: 'StyreneAWeb-BlackItalic, sans-serif',
+        StyreneAWebBold: 'StyreneAWeb-Bold, sans-serif',
+        StyreneAWebBlack: 'StyreneAWeb-Black, sans-serif',
+        StyreneAWebItalic: 'StyreneAWeb-Italic, sans-serif',
+        StyreneAWebMedium: 'StyreneAWeb-Medium, sans-serif',
+        StyreneAWebThinItalic: 'StyreneAWeb-ThinItalic, sans-serif',
+        StyreneAWebRegular: 'StyreneAWeb-Regular, sans-serif',
+        StyreneAWebLightItalic: 'StyreneAWeb-LightItalic, sans-serif',
+        StyreneAWebMediumItalic: 'StyreneAWeb-MediumItalic, sans-serif',
+        StyreneAWebThin: 'StyreneAWeb-Thin, sans-serif',
       },
+      colors: {
+        primary: {
+          1: "#2c2c3a"
+        }
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require("tw-elements/dist/plugin")
+  ],
 }
