ebabe883f9068c26af773e3cc8d6b723d5c1dfd3
Fahad Bhatti <83200973+fahadbhatti2001@users.noreply.github.com>
2024-02-13 23:58:35 +0500

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <nav className="p-4 flex justify-between items-center w-full" >
             <div className="flex gap-6">
-                <img src="/images/logo.png" className="w-14" />
+              //  <img src="/images/logo.png" className="w-14" />
                 <div className="flex justify-center items-center">
                     <button className="py-2 px-4 rounded group transition-all duration-150 ease-in-out flex items-center gap-1 bg-primary-1 hover:bg-primary-2" type="button" >
                         <img src="/images/icons/casino.png" className="w-6 group-hover:saturate-100 group-hover:hue-rotate-90 group-hover:brightness-200 group-hover:contrast-200 transition-all duration-150 ease-in-out" />
