24ce5f2b78f8a72d1ef7917113b6dba934beb715
Fahad Bhatti <83200973+fahadbhatti2001@users.noreply.github.com>
2024-06-21 00:00:01 +0500

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,7 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    const use = async () => {
+    chonst use = async () => {
       await import("tw-elements");
     };
     use();
